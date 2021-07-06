dd5089783a4e031abde5976b36096f4b3214ed9c
John Mills <john.mills@yoti.com>
2021-07-06 18:37:40 +0100

    YM-23289: Includes missing URL file.

---
+++ YotiButtonSDK/Extension/Theme+URL.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2021 Yoti Ltd. All rights reserved.
+//
+
+import Foundation
+
+extension Theme {
+    var appStoreURL: URL {
+        switch self {
+            case .easyID: return URL(string: "itms-apps://itunes.apple.com/app/id1552010191")!
+            case .yoti, .yotiUK: return URL(string: "itms-apps://itunes.apple.com/app/id983980808")!
+            case .partnership: return URL(string: "https://code.yoti.com")!
+        }
+    }
+}
